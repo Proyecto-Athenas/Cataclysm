f697275b754ae2f26ee65a544c365d60f6168f46
DennisCalderon <dennis.tacna@gmail.com>
2022-08-04 10:57:57 -0500

    |DB/Char| Agregando tabla faltante a la DB Charaters

--- sql/database/db_char.sql
+++ sql/database/db_char.sql
@@ -1518,6 +1518,25 @@ LOCK TABLES `corpse` WRITE;
 
 UNLOCK TABLES;
 
+/*Table structure for table `creature_respawn` */
+
+DROP TABLE IF EXISTS `creature_respawn`;
+
+CREATE TABLE `creature_respawn` (
+  `guid` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Global Unique Identifier',
+  `respawnTime` int(10) unsigned NOT NULL DEFAULT '0',
+  `mapId` smallint(10) unsigned NOT NULL DEFAULT '0',
+  `instanceId` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Instance Identifier',
+  PRIMARY KEY (`guid`,`instanceId`),
+  KEY `idx_instance` (`instanceId`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Grid Loading System';
+
+/*Data for the table `creature_respawn` */
+
+LOCK TABLES `creature_respawn` WRITE;
+
+UNLOCK TABLES;
+
 /*Table structure for table `cronjobs` */
 
 DROP TABLE IF EXISTS `cronjobs`;
